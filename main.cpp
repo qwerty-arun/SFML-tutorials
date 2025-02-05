c969b51f243b03caffcf6d951b809a07e43424e7
qwerty-arun <arunkr.1804@gmail.com>
2025-02-05 23:36:00 +0530

    5/2/2025: read some documentation

--- main.cpp
+++ main.cpp
@@ -1,34 +1,22 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Rect.hpp>
-#include <SFML/Graphics/RectangleShape.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/ConvexShape.hpp>
 #include <SFML/System/Vector2.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Window/Keyboard.hpp>
-#include <SFML/Window/Mouse.hpp>
-#include <SFML/Window/VideoMode.hpp>
-#include <SFML/Window/WindowStyle.hpp>
-#include "animation.h"
-#include <ctime>
 #include <iostream>
-
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(512,512),"Arun's first trial",sf::Style::Resize|sf::Style::Close);
-    sf::RectangleShape player(sf::Vector2f(150.0f,150.0f)); //f stands for float here, never use Vector2 because we need to specify the types
-    /*player.setFillColor(sf::Color::Green);*/
-    player.setPosition(206.0f,206.0f);
-    sf::Texture playerTexture; //defining a texture
-    playerTexture.loadFromFile("diff_imposters.png"); // there are 12 diff imposters 4x3 
-    player.setTexture(&playerTexture); 
+    window.setFramerateLimit(60);
+    sf::CircleShape shape(50.0f,3);
+    shape.setFillColor(sf::Color(150, 50, 250));
 
-    Animation animation(&playerTexture,sf::Vector2u(3,4),0.3f);
-    float deltaTime = 0.0f;
-    sf::Clock clock;
+// set a 10-pixel wide orange outline
+shape.setOutlineThickness(10.f);
+shape.setOutlineColor(sf::Color(250, 150, 100));
+    shape.setPosition(sf::Vector2f(100.f,100.f));
     while(window.isOpen())
     {
-        deltaTime=clock.restart().asSeconds();
         sf::Event evnt;
         while(window.pollEvent(evnt))
         {
@@ -39,11 +27,8 @@ int main()
                     break;
             }
         }
-
-        animation.Update(0,deltaTime);
-        player.setTextureRect(animation.uvRect);
-        window.clear(sf::Color(150,150,150));
-        window.draw(player);
+        window.clear();
+        window.draw(shape);
         window.display();
     }
     return 0;
